4bae8924de9e150f53a1521d9f5ffba02baf3527
Alineeniyo <alineyomungeli@gmail.com>
2022-10-30 07:29:43 +0200

    Added madlib_python

---
+++ Madlib.py
@@ -0,0 +1,10 @@
+adj =input("Adjective :")
+verb1 =input("Verb 1 :")
+verb2 =input("Verb 2:")
+famous_person =input("Famous Person : ")
+
+madlib =f"I found out that computer programming is so {adj} ! It makes me so exicited all the time because \
+    I think am in love with {verb1} . Stay hydrated and {verb2} like you are {famous_person }.@"
+
+print(madlib)
+
